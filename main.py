d7eef0075f3021b656407b6fe6a2652c2908f7eb
PatTheHyruler <pmcroofplays@gmail.com>
2023-07-13 16:10:51 +0300

    Big

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os.path
 import re
 import functools
 from enum import Enum
-from typing import List, Set, Optional, Collection
+from typing import List, Set, Optional, Collection, Callable, TypeVar
 
 import requests
 
@@ -28,11 +28,15 @@ class BaseEntry:
     def games(self) -> Set['Game']:
         raise NotImplementedError()
 
+    @property
+    def mib(self):
+        return functools.reduce(lambda a, b: a + b.mib, self.games, 0)
+
 
 class UrlEntry(BaseEntry):
     def __init__(self, url: str, name: str):
         super().__init__(name)
-        self._games = set(parse_url(url))
+        self._games = set(parse_url(url, _parse_games))
 
     @property
     def games(self) -> Set['Game']:
@@ -108,20 +112,16 @@ def _parse_games(content: str) -> List[Game]:
     return [Game(m) for m in re.finditer(pattern, content)]
 
 
-def parse_file(filepath: str) -> List[Game]:
+T = TypeVar("T")
+
+
+def parse_file(filepath: str, parse_func: Callable[[str], T]) -> T:
     with open(filepath, "r") as f:
         text = f.read()
-    return _parse_games(text)
+    return parse_func(text)
 
 
-def parse_urls(*urls: str, invalidate_cache: bool = False) -> List[Game]:
-    result = []
-    for url in urls:
-        result += parse_url(url, invalidate_cache)
-    return result
-
-
-def parse_url(url: str, invalidate_cache: bool = False) -> List[Game]:
+def parse_url(url: str, parse_func: Callable[[str], T], invalidate_cache: bool = False) -> T:
     keep_characters = (' ', '.', '_')
     conformed_url = "".join(c for c in url if c.isalnum() or c in keep_characters).rstrip()
     cache_dir = "cache"
@@ -131,12 +131,13 @@ def parse_url(url: str, invalidate_cache: bool = False) -> List[Game]:
     if invalidate_cache and os.path.exists(file_path):
         os.remove(file_path)
     if os.path.exists(file_path) and os.path.isfile(file_path):
-        return parse_file(file_path)
+        return parse_file(file_path, parse_func)
     else:
+        print(f"Url '{url}' not cached, fetching")
         text = requests.get(url).text
         with open(file_path, "w") as f:
             f.write(text)
-        return _parse_games(text)
+        return parse_func(text)
 
 
 def print_entry_info(entry: BaseEntry, depth: int = 0,
@@ -166,69 +167,100 @@ def print_games_info(games: Collection[Game], name: str, depth: int = 0, newline
         print()
 
 
-def run_gba():
-    print("GBA")
-    gba_games = parse_url(
-        "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Game%20Boy%20Advance/")
+class PageEntry:
+    def __init__(self, match: re.Match):
+        self.link = match.group(G_LINK)
 
-    combined_games = []
-    combined_games += gba_games
 
-    print_games_info(combined_games)
+def parse_upper_dir_contents(base_url: str, content: str) -> List[UrlEntry]:
+    pattern = rf"<td><a href=\"(?P<{G_LINK}>[^\"]+)\"(?: title=\"(?P<{G_TITLE}>[^\"]*)\")?>[^\"]*</a></td>"
+
+    result = []
+
+    for m in re.finditer(pattern, content):
+        title_group = m.group(G_TITLE)
+        link = m.group(G_LINK)
+        if link == "../":
+            continue
+        title = title_group if title_group else link
+
+        combined_url = base_url
+        if not combined_url.endswith("/"):
+            combined_url += "/"
+        while link.startswith("/"):
+            link = link[1:]
+        combined_url += link
+
+        result.append(UrlEntry(combined_url, title))
+
+    return result
+
+
+def parse_upper_dir(url: str, name: Optional[str] = None) -> CollectionEntry:
+    entries = parse_url(url, lambda content: parse_upper_dir_contents(url, content))
+    return CollectionEntry(name if name else url, entries)
 
 
 if __name__ == '__main__':
-    atari_2600 = UrlEntry(
-        "https://myrient.erista.me/files/No-Intro/Atari%20-%202600/",
-        "Atari 2600"
-    )
+    no_intro_entry = parse_upper_dir("https://myrient.erista.me/files/No-Intro/")
+    no_intro_entry.entries.sort(key=lambda e: e.mib)
+    print_entry_info(no_intro_entry)
 
-    g3ds_reg = UrlEntry(
-        "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%203DS%20(Decrypted)/",
-        "Regular")
-    g3ds_new = UrlEntry(
-        "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20New%20Nintendo%203DS%20(Decrypted)/",
-        "New 3DS")
-    g3ds_eshop = UrlEntry(
-        "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%203DS%20(Digital)%20(CDN)/",
-        "Digital")
-    g3ds = CollectionEntry("3DS", [g3ds_reg, g3ds_eshop, g3ds_new])
+    redump_entry = parse_upper_dir("https://myrient.erista.me/files/Redump/")
+    redump_entry.entries.sort(key=lambda e: e.mib)
+    print_entry_info(redump_entry)
 
-    ds_reg = UrlEntry(
-        "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%20DS%20(Decrypted)/",
-        "Regular")
-    dsi = UrlEntry(
-        "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%20DSi%20(Decrypted)/",
-        "DSi")
-    ds_digital = UrlEntry(
-        "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%20DSi%20(Digital)/",
-        "Digital")
-    download_play = UrlEntry(
-        "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%20DS%20(Download%20Play)/",
-        "Download play")
-    ds = CollectionEntry("DS", [ds_reg, dsi, ds_digital, download_play])
-
-    gba = UrlEntry(
-        "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Game%20Boy%20Advance/",
-        "GBA")
-
-    gc = UrlEntry(
-        "https://myrient.erista.me/files/Redump/Nintendo%20-%20GameCube%20-%20NKit%20RVZ%20[zstd-19-128k]/",
-        "GameCube")
-
-    wii = UrlEntry(
-        "https://myrient.erista.me/files/Redump/Nintendo%20-%20Wii%20-%20NKit%20RVZ%20[zstd-19-128k]/",
-        "Wii")
-
-    wii_u = UrlEntry(
-        "https://myrient.erista.me/files/Redump/Nintendo%20-%20Wii%20U%20-%20WUX/",
-        "Wii U")
-
-    all_games = CollectionEntry("All", [atari_2600, gba, gc, ds, g3ds, wii, wii_u])
-
-    # Print data
-
-    print_entry_info(all_games)
+    # atari_2600 = UrlEntry(
+    #     "https://myrient.erista.me/files/No-Intro/Atari%20-%202600/",
+    #     "Atari 2600"
+    # )
+    #
+    # g3ds_reg = UrlEntry(
+    #     "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%203DS%20(Decrypted)/",
+    #     "Regular")
+    # g3ds_new = UrlEntry(
+    #     "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20New%20Nintendo%203DS%20(Decrypted)/",
+    #     "New 3DS")
+    # g3ds_eshop = UrlEntry(
+    #     "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%203DS%20(Digital)%20(CDN)/",
+    #     "Digital")
+    # g3ds = CollectionEntry("3DS", [g3ds_reg, g3ds_eshop, g3ds_new])
+    #
+    # ds_reg = UrlEntry(
+    #     "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%20DS%20(Decrypted)/",
+    #     "Regular")
+    # dsi = UrlEntry(
+    #     "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%20DSi%20(Decrypted)/",
+    #     "DSi")
+    # ds_digital = UrlEntry(
+    #     "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%20DSi%20(Digital)/",
+    #     "Digital")
+    # download_play = UrlEntry(
+    #     "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%20DS%20(Download%20Play)/",
+    #     "Download play")
+    # ds = CollectionEntry("DS", [ds_reg, dsi, ds_digital, download_play])
+    #
+    # gba = UrlEntry(
+    #     "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Game%20Boy%20Advance/",
+    #     "GBA")
+    #
+    # gc = UrlEntry(
+    #     "https://myrient.erista.me/files/Redump/Nintendo%20-%20GameCube%20-%20NKit%20RVZ%20[zstd-19-128k]/",
+    #     "GameCube")
+    #
+    # wii = UrlEntry(
+    #     "https://myrient.erista.me/files/Redump/Nintendo%20-%20Wii%20-%20NKit%20RVZ%20[zstd-19-128k]/",
+    #     "Wii")
+    #
+    # wii_u = UrlEntry(
+    #     "https://myrient.erista.me/files/Redump/Nintendo%20-%20Wii%20U%20-%20WUX/",
+    #     "Wii U")
+    #
+    # all_games = CollectionEntry("All", [atari_2600, gba, gc, ds, g3ds, wii, wii_u])
+    #
+    # # Print data
+    #
+    # print_entry_info(all_games)
 
     # print_games_info([g for g in g3ds.games if "Europe" in g.title], "Only Europe")
     # print_games_info([g for g in g3ds.games if "USA" in g.title], "Only USA")
