328de84e5599c84ead8bc73470fc18dd72ff1024
PatTheHyruler <pmcroofplays@gmail.com>
2023-07-13 04:32:23 +0300

    it nice now

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os.path
 import re
 import functools
 from enum import Enum
-from typing import List
+from typing import List, Set, Optional, Collection
 
 import requests
 
@@ -17,6 +17,38 @@ G_LINK = "link"
 G_TITLE = "title"
 G_SIZE = "size"
 G_UNIT = "unit"
+
+
+class BaseEntry:
+    def __init__(self, name: str):
+        self.name = name
+
+    @property
+    def games(self) -> Set['Game']:
+        raise NotImplementedError()
+
+
+class UrlEntry(BaseEntry):
+    def __init__(self, url: str, name: str):
+        super().__init__(name)
+        self._games = set(parse_url(url))
+
+    @property
+    def games(self) -> Set['Game']:
+        return self._games
+
+
+class CollectionEntry(BaseEntry):
+    def __init__(self, name: str, entries: List[BaseEntry]):
+        super().__init__(name)
+        self.entries = entries
+
+    @property
+    def games(self) -> Set['Game']:
+        def reduce_func(previous: Set['Game'], addend: Set['Game']) -> Set['Game']:
+            return previous.union(addend)
+
+        return functools.reduce(reduce_func, [entry.games for entry in self.entries], set())
 
 
 class Game:
@@ -102,32 +134,96 @@ def parse_url(url: str, invalidate_cache: bool = False) -> List[Game]:
         return _parse_games(text)
 
 
-def print_info(games: List[Game], name: str | None = None):
-    if name:
-        print(name)
-    print("Amount:", len(games))
-    print("Size:", round(functools.reduce(lambda a, b: a + b.gib, games, 0), 1), "GiB")
-    print()
+def print_entry_info(entry: BaseEntry, depth: int = 0,
+                     sub_entries_depth_limit: Optional[int] = None):
+    games = entry.games
+    print_games_info(games, entry.name, depth, newline=False)
+
+    if (sub_entries_depth_limit is None or depth < sub_entries_depth_limit) \
+            and isinstance(entry, CollectionEntry) and len(entry.entries) > 0:
+        for child_entry in entry.entries:
+            print_entry_info(child_entry, depth=depth + 1,
+                             sub_entries_depth_limit=sub_entries_depth_limit)
+    else:
+        print()
+
+
+def print_games_info(games: Collection[Game], name: str, depth: int = 0, newline: bool = True):
+    prefix = depth * "   "
+    print(prefix + name)
+    print(prefix + "Amount:", len(games))
+    print(prefix + "Size:", round(functools.reduce(lambda a, b: a + b.gib, games, 0), 1), "GiB")
+    if newline:
+        print()
+
+
+def run_gba():
+    print("GBA")
+    gba_games = parse_url(
+        "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Game%20Boy%20Advance/")
+
+    combined_games = []
+    combined_games += gba_games
+
+    print_games_info(combined_games)
 
 
 if __name__ == '__main__':
-    regular_games = parse_url("https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%203DS%20(Decrypted)/")
-    new_3ds_games = parse_url("https://myrient.erista.me/files/No-Intro/Nintendo%20-%20New%20Nintendo%203DS%20(Decrypted)/")
-    eshop_games = parse_url("https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%203DS%20(Digital)%20(CDN)/")
+    g3ds_reg = UrlEntry(
+        "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%203DS%20(Decrypted)/",
+        "Regular")
+    g3ds_new = UrlEntry(
+        "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20New%20Nintendo%203DS%20(Decrypted)/",
+        "New 3DS")
+    g3ds_eshop = UrlEntry(
+        "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%203DS%20(Digital)%20(CDN)/",
+        "Digital")
+    g3ds = CollectionEntry("3DS", [g3ds_reg, g3ds_eshop, g3ds_new])
 
-    combined_games = []
-    combined_games += regular_games
-    combined_games += new_3ds_games
-    combined_games += eshop_games
+    ds_reg = UrlEntry(
+        "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%20DS%20(Decrypted)/",
+        "Regular")
+    dsi = UrlEntry(
+        "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%20DSi%20(Decrypted)/",
+        "DSi")
+    ds_digital = UrlEntry(
+        "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%20DSi%20(Digital)/",
+        "Digital")
+    download_play = UrlEntry(
+        "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%20DS%20(Download%20Play)/",
+        "Download play")
+    ds = CollectionEntry("DS", [ds_reg, dsi, ds_digital, download_play])
 
-    print_info(combined_games, "All")
-    print_info([g for g in combined_games if "Europe" in g.title], "Only Europe")
-    print_info([g for g in combined_games if "USA" in g.title], "Only USA")
-    print_info([g for g in combined_games if "Japan" not in g.title], "No Japan")
-    print_info([g for g in combined_games if g.gib < 1], "Only games < 1 GiB")
-    print_info([g for g in combined_games if g.gib < 0.7], "Only games < 0.7 GiB")
-    print_info([g for g in combined_games if g.gib < 0.4], "Only games < 0.4 GiB")
-    print_info(eshop_games, "Only eshop games")
-    print_info(regular_games + new_3ds_games, "Only non-eshop games")
-    print_info([g for g in combined_games if "(Demo)" in g.title], "Only demos")
-    print_info([g for g in combined_games if "Doko Demo Honya-san" in g.title], "Only \"Doko Demo Honya-san\" games (wtf)")
+    gba = UrlEntry(
+        "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Game%20Boy%20Advance/",
+        "GBA")
+
+    gc = UrlEntry(
+        "https://myrient.erista.me/files/Redump/Nintendo%20-%20GameCube%20-%20NKit%20RVZ%20[zstd-19-128k]/",
+        "GameCube")
+
+    wii = UrlEntry(
+        "https://myrient.erista.me/files/Redump/Nintendo%20-%20Wii%20-%20NKit%20RVZ%20[zstd-19-128k]/",
+        "Wii")
+
+    wii_u = UrlEntry(
+        "https://myrient.erista.me/files/Redump/Nintendo%20-%20Wii%20U%20-%20WUX/",
+        "Wii U")
+
+    all_games = CollectionEntry("All", [gba, gc, ds, g3ds, wii, wii_u])
+
+    # Print data
+
+    print_entry_info(all_games)
+
+    # print_games_info([g for g in g3ds.games if "Europe" in g.title], "Only Europe")
+    # print_games_info([g for g in g3ds.games if "USA" in g.title], "Only USA")
+    # print_games_info([g for g in g3ds.games if "Japan" not in g.title], "No Japan")
+    # print_games_info([g for g in g3ds.games if g.gib < 1], "Only games < 1 GiB")
+    # print_games_info([g for g in g3ds.games if g.gib < 0.7], "Only games < 0.7 GiB")
+    # print_games_info([g for g in g3ds.games if g.gib < 0.4], "Only games < 0.4 GiB")
+    # print_games_info(g3ds_eshop.games, "Only eshop games")
+    # print_games_info(set.union(g3ds_reg.games, g3ds_new.games), "Only non-eshop games")
+    # print_games_info([g for g in g3ds.games if "(Demo)" in g.title], "Only demos")
+    # print_games_info([g for g in g3ds.games if "Doko Demo Honya-san" in g.title],
+    #                  "Only \"Doko Demo Honya-san\" games (wtf)")
